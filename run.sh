4c4788d535459dc5d6ac9b0ce4f31733e6cd74e7
kitsune-udon <kitsune.udon.delicious@gmail.com>
2020-10-09 06:54:45 +0900

    fix helper shell script

--- run.sh
+++ run.sh
@@ -1,12 +1,16 @@
+#!/usr/bin/env bash
+
 PROG=$1
-TOTAL_BS=$2
-COMMON_ARGS="--seed=1 \
-             --gpus=1 \
-             --max_epochs=40 \
-             --num_workers=4 \
-             --train_batch_size=$TOTAL_BS \
-             --val_batch_size=$TOTAL_BS \
-              --learning_rate=0.03 \
-              --weight_decay=4e-4"
-MASTER_ADDR=localhost MASTER_PORT=12345 \
-  python3 $PROG $COMMON_ARGS
+
+BATCHSIZE=$2
+
+ARGS="--seed=1 \
+      --gpus=1 \
+      --max_epochs=40 \
+      --num_workers=4 \
+      --train_batch_size=$BATCHSIZE \
+      --val_batch_size=$BATCHSIZE \
+      --learning_rate=0.03 \
+      --weight_decay=4e-4"
+
+python3 $PROG $ARGS
